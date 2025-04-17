cdb6af8d9046a1821a2335ad2f0ff5b9bbb45823
Kelsey Jackson <kelseythej@gmail.com>
2025-04-17 07:32:18 -0500

    working on the webharp

---
+++ webharp/components/app/app.js
@@ -0,0 +1,14 @@
+import WebHarpStrings from "../strings/strings.js";
+
+export default class WebHarpApp extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <webharp-strings strings="${this.getAttribute(
+      "strings"
+    )}"></webharp-strings>
+    `;
+  }
+}
+if (!customElements.get("webharp-app")) {
+  customElements.define("webharp-app", WebHarpApp);
+}
